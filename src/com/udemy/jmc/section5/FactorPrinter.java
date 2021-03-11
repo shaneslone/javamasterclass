bccd792b17d35da6dacc3a3d3ab490f8713c142c
Shane Slone <slone.shane@gmail.com>
2021-03-10 19:52:16 -0500

    added solution for factor printer

---
+++ src/com/udemy/jmc/section5/FactorPrinter.java
@@ -0,0 +1,14 @@
+package com.udemy.jmc.section5;
+
+public class FactorPrinter {
+    public static void printFactors(int num){
+        if (num < 1){
+            System.out.println("Invalid Value");
+        }
+        for (int i = 1; i <= num; i++){
+            if (num % i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
