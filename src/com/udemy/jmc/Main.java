42cbea0e91c20ab664bae971646577fd246e0b53
Shane Slone <slone.shane@gmail.com>
2021-03-21 16:21:23 -0400

    added solution to diagonal star

--- src/com/udemy/jmc/Main.java
+++ src/com/udemy/jmc/Main.java
@@ -1,12 +1,12 @@
 package com.udemy.jmc;
 
 
-import static com.udemy.jmc.section5.PaintJob.getBucketCount;
+import static com.udemy.jmc.section5.DiagonalStar.printSquareStar;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(getBucketCount(11.4, 1.5));
+        printSquareStar(10);
     }
 
 }
