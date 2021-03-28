84648d52ce148c7d12450bc66ac64ab9b9d2e001
Shane Slone <slone.shane@gmail.com>
2021-03-28 15:00:26 -0400

    added solution to complex number

---
+++ src/com/udemy/jmc/section6/ComplexNumber.java
@@ -0,0 +1,38 @@
+package com.udemy.jmc.section6;
+
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public void add(double real, double imaginary){
+        this.real += real;
+        this.imaginary += imaginary;
+    }
+
+    public void add(ComplexNumber cn){
+       add(cn.getReal(), cn.getImaginary());
+    }
+
+    public void subtract(double real, double imaginary){
+        this.real -= real;
+        this.imaginary -= imaginary;
+    }
+
+    public void subtract(ComplexNumber cn){
+        subtract(cn.getReal(), cn.getImaginary());
+    }
+
+}
