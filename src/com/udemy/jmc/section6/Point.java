a4eeaed6bfa4800bb70a696d5020e3714e9b254d
Shane Slone <slone.shane@gmail.com>
2021-03-28 14:31:48 -0400

    added solution for point)

---
+++ src/com/udemy/jmc/section6/Point.java
@@ -0,0 +1,43 @@
+package com.udemy.jmc.section6;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double distance(){
+        return distance(0, 0);
+    }
+
+    public double distance(int x, int y){
+        return Math.sqrt((x - this.x) * (x - this.x) + (y - this.y) * (y - this.y));
+    }
+
+    public double distance(Point p){
+        return Math.sqrt((p.getX() - x) * (p.getX() - x) + (p.getY() - y) * (p.getY() - y));
+    }
+}
+
