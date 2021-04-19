afeed77b5c84349de8230deb0599abb0c88e529b
Shane Slone <slone.shane@gmail.com>
2021-04-19 00:22:10 -0400

    added solution to playlist challenge

--- src/com/udemy/jmc/Main.java
+++ src/com/udemy/jmc/Main.java
@@ -1,11 +1,9 @@
 package com.udemy.jmc;
 
-import static com.udemy.jmc.section8.MinimumElement.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(findMin(readElements(readInteger())));
+
     }
 
 }
